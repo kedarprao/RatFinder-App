9a3aa3ecf69d7e8233fdc3365ec966c06ef54240
William Lim <wlim@lawn-128-61-42-66.lawn.gatech.edu>
2017-09-07 16:49:28 -0400

    Revert "Finished 'calc' method in Person3.java"
    This reverts commit 5150accc42f139c111167a5ac222f390059722f1.

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -2,15 +2,15 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 3
  *  returns their name and a
- *  reversed string
- *
+ *  reversed string 
+ *  
  *  @author  Bob
  *  @version 1.1
  */
 public class Person3 {
-   /** Holds the persons real name */
+   /** Holds the persons real name */  
 	private String name;
-
+	
 	/**
 	 * The constructor, takes in the persons
 	 * name
@@ -19,19 +19,19 @@ public class Person3 {
 	public Person3(String pname){
 	  name = pname;
 	}
-
+	
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the
+	 * @return the string representing the 
 	 *         object
 	 */
 	public String toString(String input) {
 	  return name + calc(input);
 	}
-
+	
 	/**
 	 * This method should take the string
 	 * input and return its reverse.
@@ -42,11 +42,7 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  String ret = "";
-	  for (int i = input.length() - 1; i > -1; i--) {
-	    ret += input.charAt(i);
-	  }
 	  //Person 3 put your implementation here
-	  return ret;
+	  return null;
 	}
 }
