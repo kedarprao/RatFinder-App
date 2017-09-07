2be069491d7f80c9ae45753a48c9cf692b9b98a7
Meenal Kumar <meenalk01@gmail.com>
2017-09-07 17:02:27 -0400

    Fixed gtid of p5 in AntLabMain

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -74,7 +74,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("Meenal Kumar");
-                  toPrint += p5.toString("903128552");
+                  toPrint += p5.toString("mkumar75");
 
         System.out.println(toPrint);
 
