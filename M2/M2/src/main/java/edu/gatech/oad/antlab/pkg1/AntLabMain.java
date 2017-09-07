1453eb20587b67416d3e3e5d2c6c13df8f366202
Meenal Kumar <meenalk01@gmail.com>
2017-09-07 00:48:41 -0400

    Modified lines for p5

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("gburdell4");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Meenal Kumar");
+                  toPrint += p5.toString("903128552");
 		  
         System.out.println(toPrint);
         
