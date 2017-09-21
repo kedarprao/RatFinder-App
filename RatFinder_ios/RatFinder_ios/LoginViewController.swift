2ccd653f87d1d90a6328945186c28566a498a083
kedarprao <kedarprao2000@gmail.com>
2017-09-20 20:31:37 -0400

    Added a Login Screen to RatFinder iOS

--- RatFinder_ios/RatFinder_ios/LoginViewController.swift
+++ RatFinder_ios/RatFinder_ios/LoginViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +19,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
