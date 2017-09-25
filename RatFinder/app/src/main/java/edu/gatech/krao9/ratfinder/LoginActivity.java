f82e2c046d53dd3fc53f6490e88fcc9f882f98cc
Graeme Sharpe <gsharpe9@gatech.edu>
2017-09-25 14:58:44 -0400

    Added LoginActivity and admin username and password
    Added a login and registration button to main activity that starts a loginActivity to enter username and password, if the username and password exist in validLogins hashmap then they successfully are logged in

--- RatFinder/app/src/main/java/edu/gatech/krao9/ratfinder/LoginActivity.java
+++ RatFinder/app/src/main/java/edu/gatech/krao9/ratfinder/LoginActivity.java
@@ -1,7 +1,10 @@
 package edu.gatech.krao9.ratfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button toLoginActivity = (Button) findViewById(R.id.LoginButton);
+        Button toRegistrationActivity = (Button) findViewById(R.id.RegisterButton);
+
+        toLoginActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, loginPage.class));
+            }
+        });
+
     }
 }
