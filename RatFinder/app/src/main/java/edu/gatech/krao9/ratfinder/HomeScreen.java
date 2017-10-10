845b0c4c000466ffdd3c8bcce196c000e7cbfe14
William <limwilliam23@gmail.com>
2017-10-10 11:18:14 -0400

    Added Expandable ListView, Rat Sightings is now available from home page. # Conflicts: #	RatFinder/app/src/main/res/layout/startup_screen.xml

--- RatFinder/app/src/main/java/edu/gatech/krao9/ratfinder/HomeScreen.java
+++ RatFinder/app/src/main/java/edu/gatech/krao9/ratfinder/HomeScreen.java
@@ -1,8 +1,6 @@
 package edu.gatech.krao9.ratfinder;
 
 import android.content.Intent;
-import android.util.Log;
-import android.content.res.AssetManager;
 import android.os.Bundle;
 //import android.support.design.widget.FloatingActionButton;
 //import android.support.design.widget.Snackbar;
@@ -10,26 +8,16 @@ import android.support.v7.app.AppCompatActivity;
 //import android.support.v7.widget.Toolbar;
 //import android.support.design.widget.FloatingActionButton;
 //import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 
-import models.Sighting;
-import java.io.BufferedReader;
+import adapters.SightingsExpandableListAdapter;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class HomeScreen extends AppCompatActivity {
-
-    private ArrayList<Sighting> sightings;
-
-    private ListView sightingsLV;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,43 +33,14 @@ public class HomeScreen extends AppCompatActivity {
         });
 
 
-        AssetManager am = getResources().getAssets();
-        try {
-            InputStream inputStream = am.open("Rat_Sightings.csv");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            String line;
+        SightingsExpandableListAdapter adapter = new SightingsExpandableListAdapter(this);
 
-            sightings = new ArrayList<Sighting>();
-            int counter = 0;
-            while ((line = reader.readLine()) != null) {
-                sightings.add(new Sighting(line));
-                Log.d("darn!", "first loop" + counter++);
-            }
-            reader.close();
+          
+        adapter.addSightingsFromCsv(this, "Rat_Sightings.csv");
 
 
-            String[] listItems = new String[sightings.size()];
-
-            for (int i = 0; i < listItems.length; i++) {
-                Sighting x = sightings.get(i);
-
-                listItems[i] = x.getImportantStuff();
-
-            }
-            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
-            sightingsLV = (ListView) findViewById(R.id.sightingsListView);
-            sightingsLV.setAdapter(adapter);
-
-        } catch (Exception e){
-            Log.d("darn!", e.getMessage());
-        }
-
-
+        ExpandableListView sightingsLV = (ExpandableListView) findViewById(R.id.ratListView);
+        sightingsLV.setAdapter(adapter);
 
     }
-
-
-
-
-
 }
--- RatFinder/app/src/main/java/models/Sighting.java
+++ RatFinder/app/src/main/java/models/Sighting.java
@@ -1,7 +1,7 @@
 package models;
-/**
- * Created by wlim on 10/5/17.
- */
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class Sighting {
     private String uniqueKey;
@@ -14,44 +14,81 @@ public class Sighting {
     private String latitude;
     private String longitude;
 
-
-    private int splitSize;
-
+    private LinkedHashMap<String, String> children;
 
 
-    public Sighting(String raw) {
-        String[] parts = raw.split(",");
+    public Sighting() {
+        children = new LinkedHashMap<String, String>();
+    }
 
-        splitSize = parts.length;
+    public Sighting(String csvLine) {
+        children = new LinkedHashMap<String, String>();
+
+        String[] parts = csvLine.split(",");
+        int splitSize = parts.length;
 
 
-        if (splitSize > 1) {
-            uniqueKey = parts[0];
-        }
-        if (splitSize > 1) {
-            createdDate = parts[1];
-        }
-        if (splitSize > 7) {
-            locationType = parts[7];
-        }
-        if (splitSize > 8) {
-            incidentZip = parts[8];
-        }
-        if (splitSize > 9) {
-            incidentAddress = parts[9];
-        }
-        if (splitSize > 16) {
-            city = parts[16];
-        }
-        if (splitSize > 23) {
-            borough = parts[23];
-        }
-        if (splitSize > 49) {
-            latitude = parts[49];
-        }
-        if (splitSize > 50) {
-            longitude = parts[50];
-        }
+        if (splitSize > 1) { this.setUniqueKey(parts[0]); }
+        if (splitSize > 1) { this.setCreatedDate(parts[1]); }
+        if (splitSize > 7) { this.setLocationType(parts[7]); }
+        if (splitSize > 8) { this.setIncidentZip(parts[8]); }
+        if (splitSize > 9) { this.setIncidentAddress(parts[9]); }
+        if (splitSize > 16) { this.setCity(parts[16]); }
+        if (splitSize > 23) { this.setBorough(parts[23]); }
+        if (splitSize > 49) { this.setLatitude(parts[49]); }
+        if (splitSize > 50) { this.setLongitude(parts[50]); }
+
+    }
+
+    public void setUniqueKey(String uniqueKey) {
+        this.uniqueKey = uniqueKey;
+        this.children.put("Unique Key", uniqueKey);
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+        //this.children.put("Created Date", createdDate);
+    }
+
+    public void setLocationType(String locationType) {
+        this.locationType = locationType;
+        this.children.put("Location Type", locationType);
+    }
+
+    public void setIncidentZip(String incidentZip) {
+        this.incidentZip = incidentZip;
+        this.children.put("Incident Zip", incidentZip);
+    }
+
+
+    public void setIncidentAddress(String incidentAddress) {
+        this.incidentAddress = incidentAddress;
+        //this.children.put("Incident Address", incidentAddress);
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+        this.children.put("City", city);
+    }
+
+    public void setBorough(String borough) {
+        this.borough = borough;
+        this.children.put("Borough", borough);
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+        this.children.put("Latitude", latitude);
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+        this.children.put("Longitude", longitude);
+    }
+
+    //makes a copy of the hashmap so we cant alter the children member of a Sighting by getting it
+    public LinkedHashMap<String, String> getChildren() {
+        return new LinkedHashMap<>(this.children);
     }
 
     public String getUniqueKey() {
@@ -69,9 +106,7 @@ public class Sighting {
     public String getIncidentAddress() {
         return incidentAddress;
     }
-    public String getCity() {
-        return city;
-    }
+    public String getCity() { return city; }
     public String getBorough() {
         return borough;
     }
@@ -82,22 +117,4 @@ public class Sighting {
         return longitude;
     }
 
-
-    public String getImportantStuff() {
-        String acc = "";
-
-        if ((locationType == null || locationType.isEmpty())
-                && (incidentAddress == null || incidentAddress.isEmpty())) {
-            return "Some info on this sighting is missing!";
-        }
-
-        if (locationType != null && !locationType.isEmpty()) {
-            acc += "Location Type: " + locationType + ", ";
-        }
-        if (incidentAddress != null && !incidentAddress.isEmpty()) {
-            acc += "Incident Address: " + incidentAddress;
-        }
-
-        return acc;
-    }
 }
